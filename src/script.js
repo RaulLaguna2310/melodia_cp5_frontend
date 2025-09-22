6abf12e6ee73badc30b90a16fd78323e587eb356
RaulLaguna2310 <raullaguna2310@gmail.com>
2025-09-22 17:04:31 -0300

    Adicionamos uma amostra de áudio que é tocada ao clicar no CTA na hero section. Para isso utilizamos a biblioteca FreePD.

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("section form");
   const modal = document.getElementById("confirmation-modal");
   const closeModal = document.getElementById("close-modal");
+  const ctaButton = document.getElementById("cta-button");
+  const ctaAudio = document.getElementById("cta-audio");
 
   if (form) {
     form.addEventListener("submit", (event) => {
@@ -22,6 +24,13 @@ document.addEventListener("DOMContentLoaded", () => {
   if (closeModal) {
     closeModal.addEventListener("click", () => {
       modal.classList.add("hidden");
+    });
+  }
+
+  if (ctaButton && ctaAudio) {
+    ctaAudio.volume = 0.5; // Define o volume para 50%
+    ctaButton.addEventListener("click", () => {
+      ctaAudio.play();
     });
   }
 });
